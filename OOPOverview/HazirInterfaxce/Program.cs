7a0f12cab6c6080d4bc79c4962aafc00e0f06609
turkayurkmez <turkay.urkmez@gmail.com>
2021-11-01 16:19:31 +0300

    Built-in interface workshop

---
+++ OOPOverview/HazirInterfaxce/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HazirInterfaxce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassRoom classRoom = new ClassRoom();
+            classRoom.Add(new Student { Id = 1, Age = 41, LastName = "Ürkmez", Name = "Türkay" });
+            classRoom.Add(new Student { Id = 1, Age = 41, LastName = "Osmanoğlu", Name = "Sinan" });
+            classRoom.Add(new Student { Id = 2, Age = 37, LastName = "Korkmaz", Name = "Tarık" });
+            classRoom.Add(new Student { Id = 3, Age = 25, LastName = "Galip", Name = "Hikmet" });
+            classRoom.Add(new Student { Id = 4, Age = 23, LastName = "Eski", Name = "Cansu" });
+
+            classRoom.GetStudents();
+
+            foreach (var item in classRoom)
+            {
+                Console.WriteLine($"{item.Id} {item.Name} {item.Age}");
+            }
+
+        }
+    }
+}
---
+++ OOPOverview/HazirInterfaxce/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HazirInterfaxce
+{
+    public class Student : IComparable<Student>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Student other)
+        {
+            if (this.Age < other.Age)
+            {
+                return -1;
+
+            }
+            else if (this.Age > other.Age)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+    public class ClassRoom: IEnumerable<Student>
+    {
+        List<Student> students = new List<Student>();
+        public void Add(Student student)
+        {
+            students.Add(student);
+
+        }
+
+        public IEnumerator<Student> GetEnumerator()
+        {
+            foreach (var item in students)
+            {
+               yield return  item;
+            }
+        }
+
+        public List<Student> GetStudents()
+        {
+            students.Sort();
+            return students;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
