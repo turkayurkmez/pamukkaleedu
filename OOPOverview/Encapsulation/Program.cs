a445d5364316403f31569c127572a2f6d8d531ff
turkayurkmez <turkay.urkmez@gmail.com>
2021-11-01 12:00:51 +0300

    Properties added

---
+++ OOPOverview/Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.WriteLine("Kaç yaşındasınız?");
+            int age = int.Parse(Console.ReadLine());
+            student.SetAge(age);
+
+            Console.WriteLine(student.GetAge());
+
+            student.LastName = "Ürkmez";
+            var user = new { Name = "x", Age = 5 };
+            Console.WriteLine(user.GetType().Name);  
+           
+        }
+    }
+}
---
+++ OOPOverview/Encapsulation/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+   public class Student
+    {
+        private int age;
+        public Student()
+        {
+
+            this.CreatedDate = DateTime.Now;
+        }
+
+        public void SetAge(int age)
+        {
+            if (age < 16)
+            {
+                throw new ArgumentException("Yaş değeri eksi olamaz");
+            }
+            this.age = age;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string LastName { get; set; }
+
+        public DateTime CreatedDate { get; private set; }
+
+
+
+
+
+
+
+
+    }
+}
