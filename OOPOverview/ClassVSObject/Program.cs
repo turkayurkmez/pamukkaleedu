8ce5a78e819e4c0c52c5cb617646b38dd168335a
turkayurkmez <turkay.urkmez@gmail.com>
2021-11-01 11:22:09 +0300

    Class VS Object

---
+++ OOPOverview/ClassVSObject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ClassVSObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Name = "Türkay";
+
+            Student student2 = student1;
+
+            student2.Name = "Kadir";
+
+            Console.WriteLine(student1.Name);
+
+
+
+        }
+    }
+}
---
+++ OOPOverview/ClassVSObject/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassVSObject
+{
+    public class Student
+    {
+        public string Name;
+    }
+}
