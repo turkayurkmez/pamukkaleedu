9f79846653600e95f1b178bf4bfdf079a01b8a49
turkayurkmez <turkay.urkmez@gmail.com>
2021-11-02 14:58:39 +0300

    DIP added.

---
+++ SOLID/DependencyInversion/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace DependencyInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Büyük sistemler küçüklere değil; küçükler büyüklere bağlı olmalı.
+            ReportGenerator reportGenerator = new ReportGenerator(new TelegramSender());
+            reportGenerator.Send();
+            //elektrik.Yak(odadakiLambayi)
+
+        }
+
+
+    }
+
+    public class ReportGenerator
+    {
+        private readonly ISender sender;
+
+        public ReportGenerator(ISender sender)
+        {
+            this.sender = sender;
+        }
+        public void Send()
+        {
+            
+            sender.Send();
+        }
+    }
+    public interface ISender
+    {
+        void Send();
+    }
+    public class MailSender:ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Mail gönderildi");
+        }
+    }
+
+    public class WhatsappSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Whatsapp ile gönderildi");
+        }
+    }
+    public class TelegramSender : ISender
+    {
+        public void Send()
+        {
+            Console.WriteLine("Telegram uygulaması ile gönderildi");
+        }
+    }
+}
