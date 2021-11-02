f672afed0a77f2fe4606824739425d55968a1350
turkayurkmez <turkay.urkmez@gmail.com>
2021-11-02 15:43:53 +0300

    Creating common design patterns

---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextFileHelper textFileHelper = TextFileHelper.CreateInstance();
+            textFileHelper.Length = 8;
+
+            TextFileHelper textFileHelper1 = TextFileHelper.CreateInstance();
+            Console.WriteLine(textFileHelper.Length);
+            Console.WriteLine(textFileHelper1.Length);
+
+
+        }
+    }
+
+    
+
+    public class TextFileHelper
+    {
+        private TextFileHelper()
+        {
+
+        }
+
+        private static TextFileHelper existingInstance;
+        public static TextFileHelper CreateInstance()
+        {
+            if (existingInstance == null)
+            {
+                existingInstance = new TextFileHelper();
+            }
+            return existingInstance;
+        }
+        public int Length { get; set; }
+        public void Read()
+        {
+
+        }
+        public void Write()
+        {
+
+        }
+    }
+}
